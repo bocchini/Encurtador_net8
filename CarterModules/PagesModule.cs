fea21d443e192c4e14fdf5c0665cae8a04e63884
Americo Bocchini <albocchini@gmail.com>
2024-07-05 23:25:44 -0300

    Add project files.

---
+++ CarterModules/PagesModule.cs
@@ -0,0 +1,14 @@
+﻿using Carter;
+
+namespace Encurtador.CarterModules;
+
+public class PagesModule : ICarterModule
+{
+
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        Get(app);
+    }
+
+    private static void Get(IEndpointRouteBuilder app) => app.MapGet("/", async () => ("wwwroot/index.html"));
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+using Carter;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+
+var app = builder.Build();
+
+
+app.MapCarter();
+
+app.Run();
